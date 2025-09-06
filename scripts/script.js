04f279fcff6332a9640130573b40b99bdd883e0b
sifat-arch <sifatullah1004@gmail.com>
2025-09-07 01:40:59 +0600

    added all cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const categoryContainer = document.getElementById("category-container");
 const cardContainer = document.getElementById("card-container");
+const cartCardContainer = document.getElementById("cartCard-container");
+const totalAmount = document.getElementById("total-amount");
 
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
@@ -48,7 +50,7 @@ const showCardCategories = (data) => {
   cardContainer.innerHTML = "";
   data.plants.forEach((item) => {
     cardContainer.innerHTML += `
-          <div class="bg-white p-4">
+          <div id="${item.id}" class="bg-white p-4">
               <div>
                 <img src="${item.image}" alt="" class="h-48 w-full object-cover rounded-md"/>
               </div>
@@ -77,5 +79,61 @@ const showCardCategories = (data) => {
       `;
   });
 };
+
+let cartArr = [];
+
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.innerText === "Add to Cart") {
+    findCardInfo(e);
+  }
+});
+
+const findCardInfo = (e) => {
+  const title = e.target.parentNode.children[0].innerText;
+  const price = parseInt(
+    e.target.parentNode.children[2].children[1].innerText.slice(1)
+  );
+  const id = e.target.parentNode.parentNode.id;
+
+  cartArr.push({
+    title: title,
+    price: price,
+    id: id,
+  });
+
+  showCartCards(cartArr);
+};
+
+const showCartCards = (arr) => {
+  cartCardContainer.innerHTML = "";
+  let total = 0;
+
+  arr.forEach((item) => {
+    cartCardContainer.innerHTML += `
+            <div
+                class="bg-[#DCFCE7] flex justify-between items-center p-3 rounded-lg"
+              >
+                <div>
+                  <h3 class="text-sm font-semibold">${item.title}</h3>
+                  <p class="text-gray-500">৳<span>${item.price}</span> × 1</p>
+                </div>
+
+                <div>
+                  <i onclick=deleteCart('${item.id}') class="fa-solid fa-xmark text-gray-500 cursor-pointer"></i>
+                </div>
+              </div>
+      `;
+
+    total += Number(item.price);
+  });
+  totalAmount.innerText = total;
+};
+
+const deleteCart = (id) => {
+  const filterDelete = cartArr.filter((item) => item.id !== id);
+  cartArr = filterDelete;
+  showCartCards(cartArr);
+};
+
 loadAllCards();
 loadCategories();
